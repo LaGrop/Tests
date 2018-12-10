0674efb3e251530ff79b86d6f3a78fe753426770
La Grop <latringrop@gmail.com>
2018-12-10 22:14:30 +0100

    Added some readme info

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Tests
+
+Sublime
+- Anaconda
+- Bracket...
+- Sidebar Enhancment
+- Predawn
+- Material theme
+
+Syncing with Git
+https://packagecontrol.io/docs/syncing
+
+Sublime & Python
+Corey Schafer
+https://www.youtube.com/watch?v=xFciV6Ew5r4
--- test1/FirstTest.py
+++ test1/FirstTest.py
@@ -1,13 +1,14 @@
 import unittest
 
+
 class FirstTest(unittest.TestCase):
 
-	def test_a(self):
-		self.assertEqual(0,0)
+    def test_a(self):
+        self.assertEqual(0, 0)
+
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
+{
 
-
-
-
+}

81f0bb562f9c12beef4b0376dfa97182bb2a78a2
La Grop <latringrop@gmail.com>
2018-12-15 22:34:32 +0100

    more stuf

---
+++ test1/info.py
@@ -0,0 +1,29 @@
+name = 'John'
+
+# print(type(name))
+# print(dir(name))
+# print(help(str))
+# print(help(str.lower))
+
+# Arithmetic Operators:
+# Addition:       3 + 2
+# Subtraction:    3 - 2
+# Multiplication: 3 * 2
+# Division:       3 / 2
+# Floor Division: 3 // 2  (python3 only)
+# Exponent:       3 ** 2
+# Modulus:        3 % 2
+
+
+# Comparisons:
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
+
+
+a = 35000
+print(dir(a))
+print(a.bit_length())
